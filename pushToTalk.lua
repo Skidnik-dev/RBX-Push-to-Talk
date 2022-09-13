f1b3aef79d3086792505df03b656bd48125bccf7
Skidnik-dev <81454502+Skidnik-dev@users.noreply.github.com>
2022-09-13 00:56:56 -0400

    Update pushToTalk.lua

--- pushToTalk.lua
+++ pushToTalk.lua
@@ -1,20 +1,16 @@
 local VoiceChat = game:GetService("VoiceChatInternal");
 local UserInputService = game:GetService("UserInputService");
 
-local toggleKey = "T"; --Edit this for a different toggle key (obv)
+local toggleKey = "T";
 local enumKey = Enum.KeyCode[toggleKey];
 local vc_enabled = true; --Don't touch this
 
-UserInputService.InputBegan:Connect(function(inputObj)
+local function pushToTalk(inputObj)
     if inputObj.KeyCode ~= enumKey then return; end
 
     vc_enabled = (not vc_enabled);
     VoiceChat:PublishPause(vc_enabled);
-end)
+end
 
-UserInputService.InputEnded:Connect(function(inputObj)
-    if inputObj.KeyCode ~= enumKey then return; end
-
-    vc_enabled = (not vc_enabled);
-    VoiceChat:PublishPause(vc_enabled);
-end)
+UserInputService.InputBegan:Connect(pushToTalk);
+UserInputService.InputEnded:Connect(pushToTalk);
